87be11373b83009b1a4583a4aae2321c93df3ef3
Sérgio Campos <srabelocampos@gmail.com>
2020-09-10 23:56:01 -0300

    All implementations were made

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,15 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const { data } = await api.get('products');
+      setProducts(data);
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
@@ -65,7 +66,7 @@ const Dashboard: React.FC = () => {
                   testID={`add-to-cart-${item.id}`}
                   onPress={() => handleAddToCart(item)}
                 >
-                  <FeatherIcon size={20} name="plus" color="#C4C4C4" />
+                  <FeatherIcon size={32} name="plus" color="#2e302e" />
                 </ProductButton>
               </PriceContainer>
             </Product>
